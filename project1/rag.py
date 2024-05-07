c0768a3b3765e5536472db0f7d93a29737676f0f
mlu1 <mthandemeluleki@yahoo.com>
2024-05-07 13:34:31 +0800

    Add data preparation

--- project1/rag.py
+++ project1/rag.py
@@ -1,7 +1,118 @@
 import os
-import requests
-pdf_path = "human-nutrition.pdf"
+import fitz
+import random
+from tqdm import tqdm
+import pandas as pd
 
-if not os.path.exits(pdf_path)
-    print("PDF does not exist")
+pdf_path = "human-nutrition.pdf"
+filename = pdf_path
+
+def text_formatter(text: str) -> str:
+    cleaned_text = text.replace("\n", " ").strip() 
+    return cleaned_text
+
+
+def open_and_read_pdf(pdf_path: str) -> list[dict]:
+    """
+    Opens a PDF file, reads its text content page by page, and collects statistics.
+
+    Parameters:
+        pdf_path (str): The file path to the PDF document to be opened and read.
+
+    Returns:
+        list[dict]: A list of dictionaries, each containing the page number
+        (adjusted), character count, word count, sentence count, token count, and the extracted text
+        for each page.
+    """
+    doc = fitz.open(pdf_path)  # open a document
+    pages_and_texts = []
+    for page_number, page in tqdm(enumerate(doc)):  # iterate the document pages
+        text = page.get_text()  # get plain text encoded as UTF-8
+        text = text_formatter(text)
+        pages_and_texts.append({"page_number": page_number - 41,  # adjust page numbers since our PDF starts on page 42
+                                "page_char_count": len(text),
+                                "page_word_count": len(text.split(" ")),
+                                "page_sentence_count_raw": len(text.split(". ")),
+                                "page_token_count": len(text) / 4,  # 1 token = ~4 chars, see: https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+                                "text": text})
+    return pages_and_texts
+
+pages_and_texts = open_and_read_pdf(pdf_path=pdf_path)
+pages_and_texts[:2]
+
+df = pd.DataFrame(pages_and_texts)
+print(df.head(10))
+from spacy.lang.en import English # see https://spacy.io/usage for install instructions
+
+nlp = English()
+
+# Add a sentencizer pipeline, see https://spacy.io/api/sentencizer/ 
+nlp.add_pipe("sentencizer")
+
+# Create a document instance as an example
+doc = nlp("This is a sentence. This another sentence.")
+assert len(list(doc.sents)) == 2
+
+# Access the sentences of the document
+print(list(doc.sents))
+
+for item in tqdm(pages_and_texts):
+    item["sentences"] = list(nlp(item["text"]).sents)
+    
+    # Make sure all sentences are strings
+    item["sentences"] = [str(sentence) for sentence in item["sentences"]]
+    
+    # Count the sentences 
+    item["page_sentence_count_spacy"] = len(item["sentences"])
+
+
+num_sentence_chunk_size = 10 
+
+# Create a function that recursively splits a list into desired sizes
+def split_list(input_list: list, 
+               slice_size: int) -> list[list[str]]:
+    """
+    Splits the input_list into sublists of size slice_size (or as close as possible).
+
+    For example, a list of 17 sentences would be split into two lists of [[10], [7]]
+    """
+    return [input_list[i:i + slice_size] for i in range(0, len(input_list), slice_size)]
+
+# Loop through pages and texts and split sentences into chunks
+for item in tqdm(pages_and_texts):
+    item["sentence_chunks"] = split_list(input_list=item["sentences"],
+                                         slice_size=num_sentence_chunk_size)
+    item["num_chunks"] = len(item["sentence_chunks"])
+
+
+import re
+
+# Split each chunk into its own item
+pages_and_chunks = []
+for item in tqdm(pages_and_texts):
+    for sentence_chunk in item["sentence_chunks"]:
+        chunk_dict = {}
+        chunk_dict["page_number"] = item["page_number"]
+        
+        # Join the sentences together into a paragraph-like structure, aka a chunk (so they are a single string)
+        joined_sentence_chunk = "".join(sentence_chunk).replace("  ", " ").strip()
+        joined_sentence_chunk = re.sub(r'\.([A-Z])', r'. \1', joined_sentence_chunk) # ".A" -> ". A" for any full-stop/capital letter combo 
+        chunk_dict["sentence_chunk"] = joined_sentence_chunk
+
+        # Get stats about the chunk
+        chunk_dict["chunk_char_count"] = len(joined_sentence_chunk)
+        chunk_dict["chunk_word_count"] = len([word for word in joined_sentence_chunk.split(" ")])
+        chunk_dict["chunk_token_count"] = len(joined_sentence_chunk) / 4 # 1 token = ~4 characters
+        
+        pages_and_chunks.append(chunk_dict)
+
+# How many chunks do we have?
+len(pages_and_chunks)
+df = pd.DataFrame(pages_and_chunks)
+df.describe().round(2)
+
+min_token_length = 30
+for row in df[df["chunk_token_count"] <= min_token_length].sample(5).iterrows():
+    print(f'Chunk token count: {row[1]["chunk_token_count"]} | Text: {row[1]["sentence_chunk"]}')
+
 
