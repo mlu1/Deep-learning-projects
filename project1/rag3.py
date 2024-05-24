aba944e3dce21c5ebf35efcc59cdf7953748f09e
mlu1 <mthandemeluleki@yahoo.com>
2024-05-24 13:20:27 +0800

    Add training steps

--- project1/rag3.py
+++ project1/rag3.py
@@ -9,6 +9,7 @@ from transformers import (AutoModelForCausalLM,
         pipeline,
         logging)
 
+
 from peft import LoraConfig,PeftModel
 from trl import  SFTTrainer
 
@@ -37,7 +38,7 @@ def transform_conversation(example):
 
 transformed_dataset = dataset.map(transform_conversation)
 model_name ="NousResearch/Llama-2-7b-chat-hf"
-datase_name = "mlabonne/guanco-llama2-1k"
+dataset_name = "mlabonne/guanaco-llama2-1k"
 new_model = "Llama-2-7b-chat-finetune"
 
 lora_r = 64
@@ -80,22 +81,71 @@ save_steps =0
 logging_steps=25
 max_seq_length = None
 packing = False
-device_map = {"":0}
 
 
 ##start training
-
 dataset= load_dataset(dataset_name,split="train")
-compute_dtype = getatr(torch,bnb_4bit_compute_dtype)
+compute_dtype = getattr(torch,bnb_4bit_compute_dtype)
 
 
 bnb_config = BitsAndBytesConfig(
         load_in_4bit = use_4bit,
-        bnb_4bit_quant_type = 4bit_quant_type,
+        bnb_4bit_quant_type = bnb_4bit_quant_type,
         bnb_4bit_compute_dtype = compute_dtype,
+        bnb_4bit_use_double_quant=use_nested_quant
         )
 
 
+
+if compute_dtype == torch.float16 and use_4bit:
+    major,_ = torch.cuda.get_device_capability()
+    if major >=8:
+        print("="*80)
+        print("Your GPU supports bfloat16:")
+        print("="*80)
+
+
+model =AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        device_map = 'cpu')
+
+model.config.use_cache = False
+model.config.pretraining_tp =1
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_name,trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+
+peft_config = LoraConfig(
+        lora_alpha = lora_alpha,
+        lora_dropout =lora_dropout,
+        r=lora_r,
+        bias = "none",
+        task_type = "CAUSAL_LM")
+
+
+training_arguments = TrainingArguments(
+        output_dir = output_dir,
+        num_train_epochs = num_train_epochs,
+        per_device_train_batch_size = per_device_train_bathc_size,
+        gradient_accumulation_steps gradient_accumulation_steps,
+        optim = optim,
+        save_steps = save_steps,
+        logging_steps = logging_steps,
+        learning_rate = learning_rate,
+        weight_decay = weight_decay,
+        fp16 = fp16,
+        bf16 = bf16,
+        max_grad_norm = max_grad_norm,
+        max_steps = max_steps,
+        warmup_ratio = warmup_ratio,
+        group_by_length = group_by_length,
+        lr_scheduler_type = lr_scheduler_type,
+        report_to = "tensorboard"
+        )
 
 
 
